5cda168d7b3b1b48e564cf7a436721e2623da8d7
Anik Hossain <93430943+ani0809@users.noreply.github.com>
2026-03-18 03:09:54 +0600

    Add files via upload

---
+++ backends/storage/framework/views/ec58fe4777452794f2f0e21a0bd56c8b.php
@@ -0,0 +1,8 @@
+<section
+    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</section>
+<?php /**PATH C:\Users\SBL\Desktop\New folder (2)\22 - new laravel\project-bolt-sb1-qvflcjax new laravel\project\backend\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/section-container.blade.php ENDPATH**/ ?>
